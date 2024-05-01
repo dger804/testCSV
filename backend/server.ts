a1e54aea6dd0d0efcc53cd3f1f174c8b10ff52ad
Diego804 <git@github.com>
2024-05-01 17:06:12 -0500

    Create endpoint to upload csv file and split problems to resolve

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,18 @@ const port = 3000
 
 app.use(cors()) //enable cors
 
+app.post('api/files', async (req, res) => {
+  // extract file from request
+  // validate that we have the file
+  // validate type of file (csv)
+  // transform the file (buffer) to string
+  // transform the string to csv
+  // save the JSON db or memory
+  // return 200 with message and JSON
+
+  return res.status(200).json({ message: 'File upload successfully' })
+})
+
 app.listen(port, () => {
   console.log(`Server is running at http:localhost:${port}`)
 })

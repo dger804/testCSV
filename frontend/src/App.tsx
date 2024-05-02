8d352655c405d9bcf612dd59fae10db90a056fdc
Diego804 <git@github.com>
2024-05-01 20:46:39 -0500

    Adding upload button, handlers input-form and some console.log + styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,31 @@ import './App.css'
 
 function App() {
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [file] = event.target.files ?? []
+    console.log(file)
+  }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log("todo")
+  }
+
   return (
     <>
       <h4>Challenge: Upload CSV + Search</h4>
-      <div>
-        <input type="file" />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            onChange={handleInputChange}
+            name='file'
+            type="file"
+            accept='.csv'
+          />
+        </label>
+        <button>
+          Subir archivo
+        </button>
+      </form>
     </>
   )
 }
